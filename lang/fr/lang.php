47ab7add8650fa5e653b120f776728251a6eec15
Joël Esponde <joel@esponde.net>
2013-01-10 11:36:28 +0100

    French translation added

---
+++ lang/fr/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * French language file for talkpage plugin
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['back'] = 'Retour à la page';
+$lang['add']  = 'Démarrer une discussion';
+$lang['talk'] = 'Discuter de la page';
+
+
+//Setup VIM: ex: et ts=4 :
---
+++ lang/fr/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * english language file for talkpage plugin
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['langns'] = 'Espace de nom pour les discussions';
+
+//Setup VIM: ex: et ts=4 :
