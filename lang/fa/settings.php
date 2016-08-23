a42adaf6c75ce88ecedd6649b2e5fc1e9ff70fa4
Sam01 <m.sajad079@gmail.com>
2016-08-23 15:25:36 +0200

    translation update

---
+++ lang/fa/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['langns']                = 'فضای‌نام برای صفحه گفتگو';
+$lang['showbutton']            = 'به‌طور خودکار نشان دادن یک دکمه‌ی سازگار در قالب ؟';
