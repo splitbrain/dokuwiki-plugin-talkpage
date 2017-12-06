102069c5e5da10b94ee58e290bd09d92e721760d
Coen Eisma <info@coeneisma.nl>
2017-12-06 14:30:43 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Coen Eisma <info@coeneisma.nl>
+ */
+$lang['langns']                = 'Namespace voor de discussiepagina\'s';
+$lang['showbutton']            = 'Automatisch een knop weergeven in compatibele templates?';
