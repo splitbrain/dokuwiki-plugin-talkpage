897698fa5361c30cfb8d92d1df4b2b622429cf7c
Michael Bohn <mjbohn@gmail.com>
2017-12-09 17:40:38 +0100

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Michael Bohn <mjbohn@gmail.com>
+ */
+$lang['back']                  = 'Zurück zu Seite';
+$lang['add']                   = 'Eine Diskussion beginnen';
+$lang['talk']                  = 'Über diese Seite diskutieren';
