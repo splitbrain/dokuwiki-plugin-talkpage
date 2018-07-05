2024088298c2370ceeb590d2dff1f57dc99699c3
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 21:55:05 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['back']                  = 'Voltar para página';
+$lang['add']                   = 'Iniciar discussão';
+$lang['talk']                  = 'Discuta a página';
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['langns']                = 'Namespace para páginas de discussão';
+$lang['showbutton']            = 'Mostrar automaticamente um botão em modelos compatíveis?';
