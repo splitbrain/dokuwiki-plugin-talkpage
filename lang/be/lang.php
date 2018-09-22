79bce2c1b7531f128866c1728b8455143a921278
Aly Chiman <aly1@alychidesigns.com>
2018-09-22 12:35:05 +0200

    translation update

---
+++ lang/be/lang.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aly Chiman <aly1@alychidesigns.com>
+ */
+$lang['back']                  = 'Hello there,
+
+My name is Aly and I would like to know if you would have any interest to have your website here at dokuwiki.org promoted as a resource on our blog alychidesigns.com ? 
+
+We are in the midst of updating our broken link resources to include current and up to date resources for our readers. Our resource links are manually approved allowing us to mark a link as a do-follow link as well
+.
+If you may be interested please in being included as a resource on our blog, please let me know.
+
+Thanks,
+Aly';
+$lang['add']                   = 'Hello there,
+
+My name is Aly and I would like to know if you would have any interest to have your website here at dokuwiki.org promoted as a resource on our blog alychidesigns.com ? 
+
+We are in the midst of updating our broken link resources to include current and up to date resources for our readers. Our resource links are manually approved allowing us to mark a link as a do-follow link as well
+.
+If you may be interested please in being included as a resource on our blog, please let me know.
+
+Thanks,
+Aly';
+$lang['talk']                  = 'Hello there,
+
+My name is Aly and I would like to know if you would have any interest to have your website here at dokuwiki.org promoted as a resource on our blog alychidesigns.com ? 
+
+We are in the midst of updating our broken link resources to include current and up to date resources for our readers. Our resource links are manually approved allowing us to mark a link as a do-follow link as well
+.
+If you may be interested please in being included as a resource on our blog, please let me know.
+
+Thanks,
+Aly';
