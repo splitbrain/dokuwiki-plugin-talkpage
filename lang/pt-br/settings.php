fe54656102869f3839977f013b9dd59b565c32d8
Daniel Dias Rodrigues <danieldiasr@gmail.com>
2023-05-29 20:35:26 +0200

    translation update

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -3,7 +3,8 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
  * @author Alexandre Belchior <alexbelchior@gmail.com>
  */
-$lang['langns']                = 'Namespace para páginas de discussão';
+$lang['langns']                = 'Domínio para páginas de discussão';
 $lang['showbutton']            = 'Mostrar automaticamente um botão em modelos compatíveis?';
