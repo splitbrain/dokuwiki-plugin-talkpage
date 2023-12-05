a6fb9dde2f0ebf94f824eecfb73edc67bf3fa483
Martin Růžička <martinr@post.cz>
2023-12-05 19:25:14 +0100

    translation update

---
+++ lang/cs/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Martin Růžička <martinr@post.cz>
+ */
+$lang['back']                  = 'Zpět na stránku';
+$lang['add']                   = 'Zahájit diskusi';
+$lang['talk']                  = 'Diskuse o stránce';
---
+++ lang/cs/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Martin Růžička <martinr@post.cz>
+ */
+$lang['langns']                = 'Jmenný prostor diskusních stránek';
+$lang['showbutton']            = 'Automaticky zobrazit tlačítko v kompatibilních šablonách?';
